60398d1505b70c728d26a644124b0cadb3b25740
undefined <34956162+undefined-ux@users.noreply.github.com>
2022-08-26 23:28:11 +0800

    Delete Old Files

--- _CustomDesktopItems.bat
+++
@@ -1,6 +0,0 @@
-rem -- Remove desktop.ini
-rem del /a /q "%X%\Users\Default\Desktop\desktop.ini"
-rem del /a /q "%X%\Users\Public\Desktop\desktop.ini"
-
-rem -- no need this file if there is WinXShell.exe's UI_Shutdown
-rem del /a /q "%X%\Users\Default\Desktop\shutdown.bat"
